fe18267ec7a4a34200eae47deb3814ed6fa52dea
Leo Strobel <leo.strobel@uni-wuerzburg.de>
2022-12-06 13:31:47 +0100

    updated dependencies, No more infinite loop possibility in create agents

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.10"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("java")
     application
@@ -23,17 +23,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.locationtech.jts:jts-core:1.19.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("com.github.ajalt.clikt:clikt:3.5.0")
-    implementation("com.graphhopper:graphhopper-core:5.3")
-    implementation("org.slf4j:slf4j-simple:2.0.0")
+    implementation("com.graphhopper:graphhopper-core:6.2")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("org.openstreetmap.osmosis:osmosis-pbf:0.48.3")
     implementation("org.openstreetmap.osmosis:osmosis-areafilter:0.48.3")
     implementation("org.geotools:gt-main:27.1")
     implementation("org.geotools:gt-epsg-hsql:27.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
 }
 
 tasks.test {
