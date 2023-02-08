8c797d016e3665a1feb06084be74b3d897798a2b
Leo Strobel <leo.strobel@uni-wuerzburg.de>
2023-02-08 15:12:29 +0100

    Moved GH creation function

--- src/main/kotlin/de/uniwuerzburg/omod/core/Omod.kt
+++ src/main/kotlin/de/uniwuerzburg/omod/core/Omod.kt
@@ -1,13 +1,10 @@
 package de.uniwuerzburg.omod.core
 
 import com.graphhopper.GraphHopper
-import com.graphhopper.config.CHProfile
-import com.graphhopper.jackson.Jackson
-import com.graphhopper.routing.weighting.custom.CustomProfile
-import com.graphhopper.util.CustomModel
 import de.uniwuerzburg.omod.io.*
 import de.uniwuerzburg.omod.routing.RoutingCache
 import de.uniwuerzburg.omod.routing.RoutingMode
+import de.uniwuerzburg.omod.routing.createGraphHopper
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
@@ -184,26 +181,6 @@ class Omod(
             }
         }
         return Building.fromGeoJson(collection, geometryFactory, transformer)
-    }
-
-    private fun createGraphHopper(osmLoc: String, cacheLoc: String) : GraphHopper {
-        logger.info("Initializing GraphHopper... (If the osm.pbf is large this can take some time. " +
-                    "Change routing_mode to BEELINE for fast results.)")
-        val hopper = GraphHopper()
-        hopper.osmFile = osmLoc
-        hopper.graphHopperLocation = cacheLoc
-
-        // Custom Profile
-        val cp = CustomProfile("custom_car")
-        val configURL = Omod::class.java.classLoader.getResource("ghConfig.json")!!
-        val cm: CustomModel = Jackson.newObjectMapper().readValue(configURL, CustomModel::class.java)
-        cp.customModel = cm
-
-        hopper.setProfiles(cp)
-        hopper.chPreparationHandler.setCHProfiles(CHProfile("custom_car"))
-        hopper.importOrLoad()
-        logger.info("GraphHopper initialized!")
-        return hopper
     }
 
     private fun addODZoneInfo(odZones: List<ODZone>) : List<DummyLocation> {
---
+++ src/main/kotlin/de/uniwuerzburg/omod/routing/CreateHopper.kt
@@ -0,0 +1,28 @@
+package de.uniwuerzburg.omod.routing
+
+import com.graphhopper.GraphHopper
+import com.graphhopper.config.CHProfile
+import com.graphhopper.jackson.Jackson
+import com.graphhopper.routing.weighting.custom.CustomProfile
+import com.graphhopper.util.CustomModel
+import de.uniwuerzburg.omod.core.Omod
+
+fun createGraphHopper(osmLoc: String, cacheLoc: String) : GraphHopper {
+    logger.info("Initializing GraphHopper... (If the osm.pbf is large this can take some time. " +
+            "Change routing_mode to BEELINE for fast results.)")
+    val hopper = GraphHopper()
+    hopper.osmFile = osmLoc
+    hopper.graphHopperLocation = cacheLoc
+
+    // Custom Profile
+    val cp = CustomProfile("custom_car")
+    val configURL = Omod::class.java.classLoader.getResource("ghConfig.json")!!
+    val cm: CustomModel = Jackson.newObjectMapper().readValue(configURL, CustomModel::class.java)
+    cp.customModel = cm
+
+    hopper.setProfiles(cp)
+    hopper.chPreparationHandler.setCHProfiles(CHProfile("custom_car"))
+    hopper.importOrLoad()
+    logger.info("GraphHopper initialized!")
+    return hopper
+}
