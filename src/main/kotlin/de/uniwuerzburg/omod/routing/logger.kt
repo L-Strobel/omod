4078ade99f9dca70fcdbab37fdda4d3d1c687ede
Leo Strobel <leo.strobel@uni-wuerzburg.de>
2023-06-28 14:40:44 +0200

    logger.t documentation

--- src/main/kotlin/de/uniwuerzburg/omod/routing/logger.kt
+++ src/main/kotlin/de/uniwuerzburg/omod/routing/logger.kt
@@ -3,5 +3,5 @@ package de.uniwuerzburg.omod.routing
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-
+// Global logger
 val logger: Logger = LoggerFactory.getLogger("de.uniwuerzburg.omod.routing")
