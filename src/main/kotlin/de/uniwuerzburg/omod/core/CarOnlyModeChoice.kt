a9916764cff79a796f8a06abf85a7cff0ccab787
les29rq <leo.strobel@uni-wuerzburg.de>
2024-08-27 09:09:01 +0200

    Pass rng to car mode choice

--- src/main/kotlin/de/uniwuerzburg/omod/core/CarOnlyModeChoice.kt
+++ src/main/kotlin/de/uniwuerzburg/omod/core/CarOnlyModeChoice.kt
@@ -3,8 +3,6 @@ package de.uniwuerzburg.omod.core
 import com.graphhopper.GraphHopper
 import de.uniwuerzburg.omod.core.models.*
 import de.uniwuerzburg.omod.routing.Route
-import de.uniwuerzburg.omod.routing.routeFallback
-import de.uniwuerzburg.omod.routing.routeWith
 import de.uniwuerzburg.omod.utils.ProgressBar
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
@@ -28,9 +26,10 @@ class CarOnlyModeChoice(
         for (chunk in agents.chunked(10000)) { // Don't launch to many coroutines at once
             runBlocking(dispatcher) {
                 for (agent in chunk) {
+                    val coroutineRng = Random(mainRng.nextLong())
                     launch(dispatcher) {
                         for (diary in agent.mobilityDemand) {
-                            diary.visitTrips( ::tripVisitor )
+                            tripsToCar(diary, coroutineRng)
                         }
                         val done = jobsDone.incrementAndGet()
                         print("Mode Choice: ${ProgressBar.show(done / totalJobs)}\r")
@@ -40,24 +39,30 @@ class CarOnlyModeChoice(
         }
         println("Mode Choice: " + ProgressBar.done())
         logger.info("Mode Choice took: ${timeSource.markNow() - timestampStartInit}")
-        return  agents
+        return agents
     }
 
-    private fun tripVisitor(
-        trip: Trip, originActivity: Activity, destinationActivity: Activity,
-        @Suppress("UNUSED_PARAMETER") departureTime: LocalTime,
-        @Suppress("UNUSED_PARAMETER") departureWD: Weekday,
-        @Suppress("UNUSED_PARAMETER") finished: Boolean
-    )  {
-        val route = Route.getWithFallback(
-            Mode.CAR_DRIVER, originActivity.location, destinationActivity.location,
-            hopper, withPath, null, null
-        )
+    private fun tripsToCar(diary: Diary, rng: Random) {
+        val visitor = {
+                trip: Trip, originActivity: Activity, destinationActivity: Activity,
+                _: LocalTime, _: Weekday, _: Boolean ->
+            val route = if ((originActivity.type == ActivityType.HOME) && (destinationActivity.type == ActivityType.HOME)) {
+                val hhDistance = Route.sampleDistanceHomeHomeTrip(rng)
+                Route.routeFallbackFromDistance(Mode.CAR_DRIVER, hhDistance)
+            } else {
+                Route.getWithFallback(
+                    Mode.CAR_DRIVER, originActivity.location, destinationActivity.location,
+                    hopper, withPath, null, null
+                )
+            }
 
-        trip.mode = Mode.CAR_DRIVER
-        trip.time = route.time
-        trip.distance = route.distance
-        trip.lats = route.lats
-        trip.lons = route.lons
+            trip.mode = Mode.CAR_DRIVER
+            trip.time = route.time
+            trip.distance = route.distance
+            trip.lats = route.lats
+            trip.lons = route.lons
+        }
+
+        diary.visitTrips(visitor)
     }
 }
