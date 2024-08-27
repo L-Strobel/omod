9a98d864151ac07c4ab643e0585cc0fefec9b4ad
les29rq <leo.strobel@uni-wuerzburg.de>
2024-08-27 08:47:32 +0200

    Refracture routing, add home-home trip distance sampling

--- src/main/kotlin/de/uniwuerzburg/omod/routing/Route.kt
+++ src/main/kotlin/de/uniwuerzburg/omod/routing/Route.kt
@@ -7,6 +7,8 @@ import de.uniwuerzburg.omod.core.models.LocationOption
 import de.uniwuerzburg.omod.core.models.Mode
 import de.uniwuerzburg.omod.core.models.RealLocation
 import java.time.Instant
+import java.util.*
+import kotlin.math.ln
 
 class Route (
     val distance: Double,           // Unit: kilometer
@@ -61,6 +63,30 @@ class Route (
                 lats,
                 lons
             )
+        }
+
+        fun routeFallback(mode: Mode, origin: LocationOption, destination: LocationOption): Route {
+            val beelineDistance = calcDistanceBeeline(origin, destination) / 1000
+            return routeFallbackFromDistance(mode, beelineDistance)
+        }
+
+        /**
+         * @param mode Travel mode
+         * @param distance Distance of trip. Unit: Kilometer
+         */
+        fun routeFallbackFromDistance(mode: Mode, distance: Double): Route {
+            val time = when (mode) {
+                Mode.PUBLIC_TRANSIT -> distance / 22.5 * 60 // 22.5 km/h
+                Mode.FOOT -> distance / 5 * 60 // 5 km/h
+                Mode.BICYCLE -> distance / 18 * 60 // 18 km/h
+                else -> distance / 75  * 60 // 75 km/h + 5 min for Parking
+            }
+            return Route(distance, time, null, null)
+        }
+
+        fun sampleDistanceHomeHomeTrip(rng: Random): Double {
+            val lambda = 1.0 / 12.0 // TODO
+            return 1 / (-lambda) * ln(1-rng.nextDouble())
         }
     }
 }
