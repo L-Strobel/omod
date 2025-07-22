51dee12962ee4dbcc830853252a0990ec7fd4763
les29rq <leo.strobel@uni-wuerzburg.de>
2025-07-22 16:38:41 +0200

    Use omod_cache for tmp test files

--- src/test/kotlin/de/uniwuerzburg/omod/io/GTFSKtTest.kt
+++ src/test/kotlin/de/uniwuerzburg/omod/io/GTFSKtTest.kt
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.locationtech.jts.geom.Envelope
 import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
 class GTFSKtTest {
+    val tmpDirName = "omod_cache"
     val bbox_germany_small= Envelope(49.787, 49.79,9.92, 9.93)
     val bbox_korea_small= Envelope(36.3435, 36.345, 127.38,127.40)
+
+    init {
+        File(tmpDirName).mkdir()
+    }
 
     @Test
     fun clipGTFSGermanTest(){
         val input = Paths.get(javaClass.getResource("/clippedGTFSGermanyBig")!!.toURI())
         val expectedFolder = File(javaClass.getResource("/clippedGTFSGermanySmall")!!.toURI())
 
-        val outputBaseDir = File("build/test-output").apply { mkdirs() }
+        val outputBaseDir = File("$tmpDirName/test-output").apply { mkdirs() }
         val actualClippedFolder = File(outputBaseDir, "clippedGTFS")
         clipGTFSTest(input, expectedFolder, outputBaseDir,actualClippedFolder, bbox_germany_small)
     }
@@ -27,7 +33,7 @@ class GTFSKtTest {
     fun clipGTFSKoreanTest(){
         val input = Paths.get(javaClass.getResource("/clippedGTFSKoreaBig")!!.toURI())
         val expectedFolder = File(javaClass.getResource("/clippedGTFSKoreaSmall")!!.toURI())
-        val outputBaseDir = File("build/test-output").apply { mkdirs() }
+        val outputBaseDir = File("$tmpDirName/test-output").apply { mkdirs() }
         val actualClippedFolder = File(outputBaseDir, "clippedGTFS")
         clipGTFSTest(input, expectedFolder, outputBaseDir,actualClippedFolder, bbox_korea_small)
     }
